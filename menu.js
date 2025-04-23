cf9330193a76e59e57b8eecb2629b71bd0aec9a2
3DCREATIONSS <jp295814@gmail.com>
2025-04-23 12:14:55 -0300

    v2

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav ul");
-
-    menuToggle.addEventListener("click", function () {
-        navMenu.classList.toggle("show");
-    });
-});
